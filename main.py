bc0fda5ae15c260ae2cf2943a7f1efadde734091
Ianc <ianksamacs@gmail.com>
2022-10-26 22:43:06 -0300

    Mudei o comentario

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    #alguma coisa 2
+    #Mais um teste
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
