318d2c1c68ba99d400d982ce3ac38f949582723e
duasck <edu0017@live.com>
2022-10-26 23:25:46 -0300

    Criando + Classes

--- Cliente.py
+++ Cliente.py
@@ -1,1 +1,6 @@
-Cliente(nome, telefone, cpf, Endereço)
+class Cliente:
+    def __init__(self, nome = None, telefone = None, cpf= None, endereço = None):
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.endereço = endereço
